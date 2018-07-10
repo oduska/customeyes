5ab8f6cb43f2d487023d01be0578fdce0072ee01
oduska <eric@erichusted.com>
2018-07-09 20:47:31 -0600

    Made CSS inline in JS;

--- customeyes.js
+++ customeyes.js
@@ -1,3 +1,6 @@
+// Styles
+var css = '.customeyes-files .single-item:not(:first-child){text-align:right;position:relative;min-height:24px}.customeyes-files .single-item:not(:first-child) a{text-align:left;position:absolute;left:0;min-height:24px;padding-top:3px}.customeyes-files{white-space:unset}.customeyes-files .size{width:5em;display:inline-block;padding-top:3px}.customeyes-toolbar{color:#cfcfcf;background-color:#2f3136;padding:1em;border-radius:.28571429rem;box-shadow:0 0 0 1px rgba(34,36,38,.22) inset,0 0 0 0 transparent}.customeyes-toolbar .option-group{display:inline-block;margin:0 2em 0 0;-webkit-touch-callout:none;-webkit-user-select:none;-khtml-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none}.customeyes-toolbar input[type=checkbox]{position:absolute;width:1px;height:1px;padding:0;margin:-1px;overflow:hidden;clip:rect(0,0,0,0);border:0}.customeyes-toolbar input[type=checkbox]:checked+label::after{display:block}.customeyes-toolbar label{cursor:pointer;position:relative}.customeyes-toolbar label::before{content:"";display:inline-block;width:16px;height:16px;vertical-align:middle;margin-right:.4em;position:relative;top:-1px;border:2px solid #616f80}.customeyes-toolbar label::after{display:none;content:"x";font-weight:700;position:absolute;left:0;top:0;width:15px;height:16px;font-size:16px;line-height:.9;background-size:contain;background-repeat:no-repeat;text-align:center}.customeyes-files.icons-enabled .single-item:not(:first-child) a{padding-left:32px}.customeyes-files.icons-enabled .customeyes-icon{display:block}.customeyes-icon{display:none;text-align:center;width:100%;max-width:24px;position:absolute;left:0;top:0;z-index:20;filter:grayscale(1);-webkit-filter:grayscale(1)}.colored-icons .customeyes-icon{filter:grayscale(0);-webkit-filter:grayscale(0)}.customeyes-files .name{width:auto;max-width:calc(100% - 15em);white-space:nowrap;text-overflow:ellipsis;overflow:hidden;z-index:1;display:block}.customeyes-files .name:focus,.customeyes-files .name:hover{overflow-x:visible;background:#2f3136;z-index:10;max-width:none;padding-right:.75em}.customeyes-files.focus-highlight .focus,.customeyes-files.focus-highlight .name:focus,.customeyes-files.focus-highlight .name:hover,.customeyes-files.focus-highlight .single-item:focus,.customeyes-files.focus-highlight .single-item:hover{background-color:#36393e}.customeyes-files .directory .size{color:transparent}';
+
 // Directory/file type icons
 var icons = '<svg style="display: none;" class="mimetype-icons" xmlns="http://www.w3.org/2000/svg">' +
             '<symbol id="disc-image" viewBox="0 0 48 48"><path d="M24 4C12.954 4 4 12.955 4 24s8.954 20 20 20 20-8.955 20-20S35.046 4 24 4zm0 23.684c-2.035 0-3.684-1.649-3.684-3.684s1.649-3.684 3.684-3.684 3.684 1.649 3.684 3.684-1.649 3.684-3.684 3.684z" fill="#90caf9"/><g fill="#cfe8f9"><path d="M28.022 22.645l10.902-8.699c-1.326-1.963-3.033-3.645-5.008-4.955l-8.759 10.925c1.348.384 2.417 1.407 2.865 2.729zM19.934 25.214L8.999 33.927c1.333 2.008 3.057 3.734 5.065 5.068l8.665-10.946c-1.344-.426-2.39-1.484-2.795-2.835z"/></g><path d="M24 18c-3.314 0-6 2.688-6 6s2.686 6 6 6c3.313 0 6-2.688 6-6s-2.687-6-6-6zm0 8c-1.104 0-2-.895-2-2s.896-2 2-2 2 .895 2 2-.896 2-2 2z" fill="#1e88e5"/></symbol>' +
@@ -7,9 +10,11 @@ var icons = '<svg style="display: none;" class="mimetype-icons" xmlns="http://ww
             '<symbol id="audio" viewBox="0 0 48 48"><g fill="#e91e63"><path d="M28 33c0 4.96875-4.03125 9-9 9s-9-4.03125-9-9 4.03125-9 9-9 9 4.03125 9 9z"/><path d="M24 6v27h4V14l11 3v-7z"/></g></symbol>' +
             '<symbol id="folder" viewBox="0 0 48 48"><path d="M40 12H22l-4-4H8c-2.199219 0-4 1.800781-4 4v8h40v-4c0-2.199219-1.800781-4-4-4z" fill="#ffa000"/><path d="M40 12H8c-2.199219 0-4 1.800781-4 4v20c0 2.199219 1.800781 4 4 4h32c2.199219 0 4-1.800781 4-4V16c0-2.199219-1.800781-4-4-4z" fill="#ffca28"/></symbol>' +
             '<symbol id="text" viewBox="0 0 48 48"><path d="M40 45H8V3h22l10 10v32z" fill="#efefef"/><path d="M38.5 14H29V4.5l9.5 9.5z" fill="#cccccc"/><path class="st3" d="M16 21h17v2H16v-2zM16 27h17v2H16v-2zM16 33h13v2H16v-2z"/></symbol>' +
+            '<symbol id="nfo" viewBox="0 0 48 48"><path d="M40 45H8V3h22l10 10v32z" fill="#90caf9"/><path d="M38.5 14H29V4.5l9.5 9.5z" fill="#e1f5fe"/><path fill="#1976d2" d="M16 21h17v2H16v-2zM16 27h17v2H16v-2zM16 33h13v2H16v-2z"/></symbol>' +
             '<symbol id="pdf" viewBox="0 0 48 48"><path d="M40 45H8V3h22l10 10v32z" fill="#ff5722"/><path d="M38.5 14H29V4.5l9.5 9.5z" fill="#fbe9e7"/><path d="M16 39c-.4 0-.7-.1-1-.2-1.1-.6-1.2-1.5-1-2.2.4-1.2 2.6-2.7 5.5-4 1.3-2.4 2.3-4.9 2.9-7-1-1.9-1.5-3.7-1.5-5 0-.7.2-1.3.5-1.8.4-.5 1-.8 1.8-.8.9 0 1.6.5 1.9 1.4.5 1.2.2 3.4-.5 5.9 1 1.7 2.2 3.3 3.5 4.5 1.9-.4 3.6-.6 4.7-.4 1.9.3 2.2 1.6 2.2 2.1 0 2.1-2.2 2.1-3 2.1-1.5 0-3-.6-4.3-1.7-2.4.6-4.8 1.4-6.7 2.3-1 1.7-2 3.1-2.9 3.9-.9.7-1.6.9-2.1.9zm1.2-2.9c-.5.3-.9.6-1.1.9.2-.1.6-.3 1.1-.9zm13.6-4.7c.4.1.8.2 1.2.2.6 0 .9-.1 1-.1-.1-.1-.8-.3-2.2-.1zm-7-3.6c-.4 1.2-1 2.5-1.5 3.7 1.2-.4 2.4-.8 3.6-1.1-.8-.8-1.5-1.7-2.1-2.6zm-.6-7.8h-.1c-.1.1-.2.8.2 2.3.1-1.2.1-2.1-.1-2.3z" fill="#fff"/></symbol>' +
             '<symbol id="ebook" viewBox="0 0 48 48"><path d="M38 5l-24-.003906c-2.207031 0-4.816406 1.070312-4.984375 6.003906H9v28l.019531-.054687C9.199219 42.585938 11.566406 43 12.605469 43H37c2 0 2-2 2-2V6c0-.554687-.445312-1-1-1z" fill="#7e57c2"/><path d="M36 36H12.605469C11.273438 36 11 37.117188 11 38.5c0 1.382813.273438 2.503906 1.605469 2.503906L36 41z" fill="#ffe0b2"/><path d="M12.605469 36c-1.164063 0-1.519531.859375-1.589844 2H36v-2z" fill="#e0b990"/><path d="M14 10.996094h21v5H14z" fill="#ffecb3"/><path d="M38 36h-2v7h1c2 0 2-2 2-2v-5.996094C39 35.554688 38.554688 36 38 36z" fill="#311b92"/></symbol>' +
             '<symbol id="windows" viewBox="0 0 48 48"><g fill="#03a9f4"><path d="M20 25H6v12.074219l14 1.917969zM20 9.101563L6 11.066406V23h14zM22 8.820313V23h20V6.011719zM22 25v14.269531l20 2.742188V25z"/></g></symbol>' +
+            '<symbol id="torrent" viewBox="0 0 48 48"><path d="M42 38c0 2.210938-1.789062 4-4 4H10c-2.210937 0-4-1.789062-4-4V10c0-2.210937 1.789063-4 4-4h28c2.210938 0 4 1.789063 4 4z" fill="#4caf50"/><path d="M15.5 19l8.5-8.5 8.5 8.5zM15.5 30l8.5 8.5 8.5-8.5z" fill="#dcedc8"/><path d="M21 17h6v15h-6z" fill="#dcedc8"/></symbol>' +
             '</svg>';
 
 // Build toolbar object
@@ -27,29 +32,52 @@ var videos = ['webm', 'mkv', 'flv', 'vob', 'ogv', 'gifv', 'mng', 'avi', 'mov', '
 discImages = ['iso', 'bin', 'cue', 'ciso', 'nrg', 'mdf', 'mds', 'img'],
     ebooks = ['cbr', 'cbz', 'cb7', 'cbt', 'cba', 'djvu', 'epub', 'fb2', 'ibook', 'azw', 'lit', 'prc', 'mobi', 'pdb'];
 
+$('pre').before('<style type="text/css">' + css + '</style>');
 
 // Add class and prepend toolbar to file list
-$('pre').addClass('customeyes').before(toolbar).before(icons);
+$('pre').addClass('customeyes-files').before(toolbar).before(icons);
 
+var $customeyesFiles = $('.customeyes-files');
 
 // Toggle toolbar options
 $('body').on('click', '#enable-icons', function() {
-    $('pre.customeyes').toggleClass('icons-enabled');
+    $customeyesFiles.toggleClass('icons-enabled');
+
+    if ( $customeyesFiles.hasClass('icons-enabled') ) {
+        localStorage.setItem('icons-enabled', true);
+    }
+    else {
+        localStorage.setItem('icons-enabled', false);
+    }
 });
 
 $('body').on('click', '#colored-icons', function() {
-    $('pre.customeyes').toggleClass('colored-icons');
+    $customeyesFiles.toggleClass('colored-icons');
+
+    if ( $customeyesFiles.hasClass('colored-icons') ) {
+        localStorage.setItem('colored-icons', true);
+    }
+    else {
+        localStorage.setItem('colored-icons', false);
+    }
 });
 
 $('body').on('click', '#focus-highlight', function() {
-    $('pre.customeyes').toggleClass('focus-highlight');
+    $customeyesFiles.toggleClass('focus-highlight');
+
+    if ( $customeyesFiles.hasClass('focus-highlight') ) {
+        localStorage.setItem('focus-highlight', true);
+    }
+    else {
+        localStorage.setItem('focus-highlight', false);
+    }
 });
 
-$('body').on('focus', '.customeyes .name', function() {
+$('body').on('focus', '.customeyes-files .name', function() {
     $(this).parent().addClass('focus');
 });
 
-$('body').on('focusout', '.customeyes .name', function() {
+$('body').on('focusout', '.customeyes-files .name', function() {
     $(this).parent().removeClass('focus');
 });
 
@@ -58,13 +86,13 @@ $('pre a:not(:first-child)').addClass('name');
 
 
 // Find and wrap date/time and file size with span tags
-$('pre').contents().filter(function() {
+$customeyesFiles.contents().filter(function() {
     return this.nodeType === 3;
 }).each(function() {
     $(this).replaceWith( $(this).text().replace(/([0-9]{1,2}-[a-zA-Z]{3}-[0-9]{4} [0-9]{2}:[0-9]{2})/, '<span class="date-time">$1</span>') );
 });
 
-$('pre').contents().filter(function() {
+$customeyesFiles.contents().filter(function() {
     return this.nodeType === 3;
 }).each(function() {
     $(this).replaceWith( $(this).text().replace(/([0-9-].*)/, '<span class="size">$1</span>') );
@@ -118,6 +146,12 @@ $('.file .name').each(function() {
         else if ( extension == 'exe' ) {
             $(this).prepend('<svg class="customeyes-icon" viewBox="0 0 48 48"><use class="windows" xlink:href="#windows" /></svg>');
         }
+        else if ( extension == 'torrent' ) {
+            $(this).prepend('<svg class="customeyes-icon" viewBox="0 0 48 48"><use class="torrent" xlink:href="#torrent" /></svg>');
+        }
+        else if ( extension == 'nfo' ) {
+            $(this).prepend('<svg class="customeyes-icon" viewBox="0 0 48 48"><use class="nfo" xlink:href="#nfo" /></svg>');
+        }
         else if ( $.inArray(extension, ebooks) != -1 ) {
             $(this).prepend('<svg class="customeyes-icon" viewBox="0 0 48 48"><use class="ebook" xlink:href="#ebook" /></svg>');
         }
@@ -138,3 +172,20 @@ $('.file .name').each(function() {
         }
     }
 });
+
+// Load settings on page load
+
+if ( localStorage.getItem('icons-enabled') == 'true' ) {
+    $('#enable-icons').prop('checked', true);
+    $customeyesFiles.addClass('icons-enabled');
+}
+
+if ( localStorage.getItem('colored-icons') == 'true' ) {
+    $('#colored-icons').prop('checked', true);
+    $customeyesFiles.addClass('colored-icons');
+}
+
+if ( localStorage.getItem('focus-highlight') == 'true' ) {
+    $('#focus-highlight').prop('checked', true);
+    $customeyesFiles.addClass('focus-highlight');
+}
